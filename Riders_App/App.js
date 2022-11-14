9508607141fcbcc4f0747c850792e7a900960822
Pratik <pratik.vernekar@robosoftin.com>
2022-11-14 14:55:36 +0530

    adding login screen

--- Riders_App/App.js
+++ Riders_App/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LoginScreen from './src/screens/LoginScreen';
 
 function HomeScreen() {
   return (
@@ -22,12 +23,12 @@ function SettingsScreen() {
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+  return (<LoginScreen/>
+    // <NavigationContainer>
+    //   <Tab.Navigator>
+    //     <Tab.Screen name="Home" component={HomeScreen} />
+    //     <Tab.Screen name="Settings" component={SettingsScreen} />
+    //   </Tab.Navigator>
+    // </NavigationContainer>
   );
 }
